843a1451b871b86df07012fc321b13ca1436a33f
laosb <laosb@lasteam.net>
2019-03-25 10:59:20 +0800

    v2.0.0-prealpha.14

---
+++ packages/core/src/components/hola-assets-cdn/hola-assets-cdn.tsx
@@ -0,0 +1,13 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'hola-assets-cdn',
+  styleUrls: [
+    'hola-assets-cdn.css'
+  ]
+})
+export class HolaBase {
+  render () {
+    return
+  }
+}
---
+++ packages/core/src/components/hola-assets-cdn/readme.md
@@ -0,0 +1,5 @@
+# hola-assets-cdn
+
+Use this component to load all assets required by Holakit from CDNs.
+
+In offline situations (users don't have Internet access), you should use `hola-assets-local`.
